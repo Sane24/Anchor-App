73b3bd32650a7616adb9c30ba282c539df6492df
Naomi Kim <naomikim0401@gmail.com>
2026-07-28 20:05:30 -0700

    Apply saved theme color before initial render

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
+import { initThemeColor } from './hooks/useThemeColor'
 import './styles/tokens.css'
 import './styles/app.css'
+
+initThemeColor()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
